a5b674bc50d2f1add6994a6cf185cfd8cbe939d3
luiz fillipe <luiz.fillipe.2000@hotmail.com>
2023-10-05 17:23:58 -0300

    Recuperando status da partida e o tempo da partida.

--- src/main/java/br/com/lf/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/lf/brasileiraoapi/util/ScrapingUtil.java
@@ -17,7 +17,7 @@ public class ScrapingUtil {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String url = BASE_URL_GOOGLE + "palmeiras+x+corinthians+08/08/2020" + COMPLEMENTO_URL_GOOGLE;
+		String url = BASE_URL_GOOGLE + "AS+Roma+Servette" + COMPLEMENTO_URL_GOOGLE;
 		
 		ScrapingUtil scraping = new ScrapingUtil();
 		scraping.obtemInformacoesPartida(url);
@@ -35,6 +35,12 @@ public class ScrapingUtil {
 				LOGGER.info("Titulo da pagina: {}", title);
 				
 				
+				StatusPartida statusPartida = obtemStatusPartida(document);
+				LOGGER.info("Status partida: {}",statusPartida);
+		
+				String tempoPartida = obtemTempoPartida(document);
+				LOGGER.info("Tempo partida: {}", tempoPartida);
+				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE COM JSOUP -> {}", e.getMessage());
@@ -43,5 +49,65 @@ public class ScrapingUtil {
 			
 			return partida;
 		
+	}
+	
+	public StatusPartida obtemStatusPartida(Document document) {
+		
+		//Situações
+		//1 - partida nao iniciada
+		//2 - partida iniciada
+		//3 - partida encerrada
+		//4 - partida penaltis 
+		StatusPartida statusPartida = StatusPartida.PARTIDA_NAO_INICIADA;
+		
+		
+		boolean isTempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").isEmpty();
+		if (!isTempoPartida) {
+			String tempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").first().text();
+			statusPartida = StatusPartida.PARTIDA_EM_ANDAMENTO;
+			
+			
+			if (tempoPartida.contains("Pênaltis")) {
+				statusPartida = StatusPartida.PARTIDA_PENALTIS;
+			}
+			//LOGGER.info(tempoPartida);
+		}
+		
+		isTempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+		
+		if (!isTempoPartida) {
+			statusPartida = StatusPartida.PARTIDA_ENCERRADA;
+		}
+		//LOGGER.info(statusPartida.toString());
+		return statusPartida;
+		
+	}
+	
+	public String obtemTempoPartida(Document document) {
+		// jogo rolando ou intervalo ou penalidades
+		String tempoPartida = null;
+		
+		boolean isTempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").isEmpty();
+		if (!isTempoPartida) {
+			tempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").first().text();
+		}
+		isTempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+		if (!isTempoPartida) {
+		 tempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").first().text();
+		}
+		
+	
+		return corrigeTempoPartida(tempoPartida);
+		
+		
+	}
+	
+	public String corrigeTempoPartida(String tempo) {
+		
+		if (tempo.contains("'")) {
+			return tempo.replace(" ", "").replace("'"," min");
+		} else {
+			return tempo;
+		}	
 	}
 }
